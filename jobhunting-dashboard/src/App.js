ba018eff99bd2b325a7e6c5f8e1f29b177797ac1
slowpeace2020 <slowpeace2020@gmail.com>
2023-12-18 10:47:16 -0500

    react test

--- jobhunting-dashboard/src/App.js
+++ jobhunting-dashboard/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import {Button, Radio} from 'antd'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button type='primary'>Hello</Button>
+            <br/>
+            <Radio.Group value='large'>
+                <Radio.Button value="large">Large</Radio.Button>
+                <Radio.Button value="default">Default</Radio.Button>
+                <Radio.Button value="small">Small</Radio.Button>
+            </Radio.Group>
+        </div>
+    );
 }
 
 export default App;
